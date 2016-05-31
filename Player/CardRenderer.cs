efcc1a279076af4025d1f1b8f1d33d7cd4500d25
giawa <giawa@hotmail.com>
2016-05-30 23:24:36 -0700

    First pass at drawing button icons - seems to be pretty decent so far

--- Player/CardRenderer.cs
+++ Player/CardRenderer.cs
@@ -67,30 +67,63 @@ namespace Player
 
             foreach (var part in background.Parts)
             {
-                Color c = (part.Type == HyperCard.PartType.Button ? Color.DodgerBlue : Color.Orange);
-
-                using (Brush brush = new SolidBrush(Color.FromArgb(100, c)))
+                if (part.Type == HyperCard.PartType.Button) RenderButton(part, pe.Graphics);
+                else
                 {
-                    pe.Graphics.FillRectangle(brush, part.Rect.Left, part.Rect.Top, part.Rect.Width, part.Rect.Height);
+                    Color c = (part.Type == HyperCard.PartType.Button ? Color.DodgerBlue : Color.Orange);
 
-                    if (part.Type == HyperCard.PartType.Button)// && part.Highlighted)
+                    using (Brush brush = new SolidBrush(Color.FromArgb(100, c)))
                     {
+                        pe.Graphics.FillRectangle(brush, part.Rect.Left, part.Rect.Top, part.Rect.Width, part.Rect.Height);
+
+                        if (part.Type == HyperCard.PartType.Button)// && part.Highlighted)
+                        {
+                        }
                     }
                 }
             }
 
             foreach (var part in card.Parts)
             {
-                Color c = (part.Type == HyperCard.PartType.Button ? Color.DodgerBlue : Color.Orange);
-
-                using (Brush brush = new SolidBrush(Color.FromArgb(100, c)))
+                if (part.Type == HyperCard.PartType.Button) RenderButton(part, pe.Graphics);
+                else
                 {
-                    pe.Graphics.FillRectangle(brush, part.Rect.Left, part.Rect.Top, part.Rect.Width, part.Rect.Height);
+                    Color c = (part.Type == HyperCard.PartType.Button ? Color.DodgerBlue : Color.Orange);
 
-                    if (part.Type == HyperCard.PartType.Button)// && part.Highlighted)
+                    using (Brush brush = new SolidBrush(Color.FromArgb(100, c)))
                     {
+                        pe.Graphics.FillRectangle(brush, part.Rect.Left, part.Rect.Top, part.Rect.Width, part.Rect.Height);
+
+                        if (part.Type == HyperCard.PartType.Button)// && part.Highlighted)
+                        {
+                        }
                     }
                 }
+            }
+        }
+
+        private void RenderButton(HyperCard.Part part, Graphics g)
+        {
+            Color c = (part.Type == HyperCard.PartType.Button ? Color.DodgerBlue : Color.Orange);
+
+            using (Brush brush = new SolidBrush(Color.FromArgb(100, c)))
+            {
+                //g.FillRectangle(brush, part.Rect.Left, part.Rect.Top, part.Rect.Width, part.Rect.Height);
+            }
+
+            if (part.IconID != 0)
+            {
+                // icons use 255, 255, 254 as their transparency key
+                ImageAttributes attr = new ImageAttributes();
+                attr.SetColorKey(Color.FromArgb(255, 255, 254), Color.FromArgb(255, 255, 254));
+
+                // try to get the resource - returns a sane default
+                var icon = Icon.PngFromID(part.IconID);
+
+                // calculate the correct position and then draw it
+                // TODO:  Need to account for text spacing once text rendering is enabled
+                Point p = new Point(part.Rect.Left + ((part.Rect.Width - icon.Width) >> 1), part.Rect.Top + ((part.Rect.Height - icon.Height) >> 1));
+                g.DrawImage(icon, new Rectangle(p, icon.Size), 0, 0, icon.Width, icon.Height, GraphicsUnit.Pixel, attr);
             }
         }
     }
